a5fef2da187137283c2833b60a38046875f69477
carlosAlcalaYepes <36190473+carlosAlcalaYepes@users.noreply.github.com>
2020-10-23 20:20:55 +0200

    css

--- partials/footer.php
+++ partials/footer.php
@@ -5,7 +5,7 @@
 	</p>
 	<address>
 		<p class="izq"> Written by
-			<a href="mailto:webmaster@example.com" rev="author">Lola</a>.</p>
+			<a href="al361981@uji.es" rev="author">Maria Jesús Prior Bruno y Carlos Alcalá Yepes</a>.</p>
 		<p class="der"> Visit us at:12006 UJI </p>
 	</address>
 </footer>
