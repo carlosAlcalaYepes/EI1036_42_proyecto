419ced5f6837c11ac1d9a15a1542174e4c9aa90c
carlosAlcalaYepes <36190473+carlosAlcalaYepes@users.noreply.github.com>
2020-10-26 15:20:18 +0100

    dudas

--- includes/autentificar_usuario.php
+++ includes/autentificar_usuario.php
@@ -2,9 +2,9 @@
 
 function autentificar_usuario()
 {
-    //Version1
+    /*version
     global $pdo;
-    $datos=$_REQUEST;
+    $datos=$_REQUEST;//$_POST
 
     $query= sprintf("SELECT * FROM cliente WHERE nombre='%s' AND contraseña='%s'", mysql_real_escape_string($datos['nombre'])
                                                                                  , mysql_real_escape_string($datos['contraseña']));
@@ -18,15 +18,14 @@ function autentificar_usuario()
         $cliente=mysql_fetch_assoc($resultado);
         $_SESSION["usuario"]=$cliente['rol'];
     }
-    //version1
+    version1*/
 
     //Version2
     global $pdo;
 
-    $datos = $_REQUEST;
+    $datos = $_POST;
 
-    $query = "SELECT * FROM cliente WHERE nombre=? AND contraseña=?
-                          VALUES (?,?)";
+    $query = "SELECT * FROM cliente WHERE nombre=? AND contraseña=?";
     try { 
         $valor = array($_REQUEST['nombre'],$_REQUEST['contraseña']);
         $a=ejecutarSQL($query,$valor);//SUpongo que es un array, pero con valor o solo indices?
@@ -36,7 +35,8 @@ function autentificar_usuario()
         }
         else{
             
-            $_SESSION["usuario"] = $a['rol'];
+            $_SESSION["usuario"] = $a[0]['rol'];
+            $_SESSION["id_usuario"] = $a[0]['id_cliente'];
         }
     
     } catch (PDOExeption $e) {
