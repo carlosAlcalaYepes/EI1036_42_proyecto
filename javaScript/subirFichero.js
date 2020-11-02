6dbd2eb09765604977e05832ffc051581184b531
al361981 <al361981@uji.es>
2020-11-02 16:58:33 +0100

    imagen

---
+++ javaScript/subirFichero.js
@@ -0,0 +1,16 @@
+function	handleFiles(e)	{
+    let ctx	=	document.getElementById('canvas').getContext('2d');
+    let img	=	new	Image;
+    img.src	=	URL.createObjectURL(e.target.files[0]);
+    img.onload	=	function()	{
+                    ctx.drawImage(img,	10,10);
+    }
+}
+
+function cerrar(){
+    document.getElementById('ventanaImagen').style.display = 'none';
+}
+
+function abrirForm(){
+    document.getElementById('ventanaImagen').style.display = 'block';
+}
--- partials/registrar_producto.php
+++ partials/registrar_producto.php
@@ -1,5 +1,7 @@
-<main>
-	<form class="form_producto" action="?action=insertar_producto" method="POST">
+
+<main class="form_producto">
+	<script type="text/javascript" src="/javaScript/subirFichero.js"></script>
+	<form  action="?action=insertar_producto" method="POST">
 
 		<legend>Datos registro</legend>
 		<br/>
@@ -15,10 +17,24 @@
 		<input type="text" name="imagen" class="item_requerid" size="20" maxlength="500" value=""
 		 placeholder="" />
 		<br/>
-	
 		<br/>
+		
 		<p><input type="submit" value="Enviar">
 		<input type="reset" value="Deshacer">
 		</p>
 	</form>
+	<br/>
+	<button type="button" onclick="abrirForm()">Subir imagen</button>
+	<div class="ventanaImagen" id="ventanaImagen" style="display:none">
+			<button type = "button" id="X" onclick="cerrar()"> X </button>
+			<form action="?action=upload" method="post" enctype="multipart/form-data">
+			Selecciona	una	imagen:
+			<br/>
+			<input onchange= "handleFiles(event)" type="file" accept="image/*" name="tmp_file" id="upload">
+			<br/>
+			<canvas id="canvas" width="100" height="100"></canvas>
+			<br/>
+			<input type="submit" value="SUBIR" name="submit">
+			</form>
+		</div>
 </main>
