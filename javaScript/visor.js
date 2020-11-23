46165648fa352dc0a756e7eb7e6937a35dc600f0
carlosAlcalaYepes <36190473+carlosAlcalaYepes@users.noreply.github.com>
2020-11-23 17:03:59 +0100

    23/11/2020

--- javaScript/visor.js
+++ javaScript/visor.js
@@ -1,7 +1,23 @@
-function visor(){
+function visor(x){
 
-    .forEach(element => {
-        
-    });
-    let nodo = document.createElement('div')
+    let contenedor=document.createElement('div')
+    contenedor.setAttribute('class', 'item')
+    contenedor.setAttribute('id',x["id_producto"])
+
+
+    let imagen=document.createElement("img")
+    imagen.setAttribute("src",x["imagen"])
+    imagen.setAttribute("width","100")
+    imagen.setAttribute("height","100")
+
+    let parrafo = document.createElement("p")
+    parrafo.textContent= x["nombre"]+" "+ x["producto"]+"€"
+
+    let boton=document.createElement("button")
+    boton.setAttribute("class", "botonleermas")
+    boton.setAttribute("type", "submit")
+    boton.setAttribute("onclick", "anyadir(this)")
+    boton.setAttribute("id", x['id_producto'])
+    boton.textContent = 'Añadir'
+
 }
